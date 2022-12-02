5f6cddaea2e58a1c338e3bfc27f04c74cc863665
Joy Vaz <joy.vaz@uga.edu>
2022-12-02 19:16:50 +0530

    update pqs and autograders

--- lessons/ls03_line_autograder.R
+++ lessons/ls03_line_autograder.R
@@ -8,6 +8,7 @@
 
 if(!require(pacman)) install.packages("pacman")
 pacman::p_load_gh('graph-courses/autograder')
+pacman::p_load_gh("KO112/KO")
 pacman::p_load(tidyverse,
                praise,
                gapminder,
@@ -23,11 +24,9 @@ pacman::p_load(tidyverse,
 ## load data ----
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-.gapminder <- gapminder
+gapminder <- gapminder::gapminder
 
-data(.gapminder, package="gapminder")
-.gap_US <- dplyr::filter(.gapminder,
-                              country == "United States")
+gap_US <- dplyr::filter(gapminder, country == "United States")
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ## q1 ----
@@ -41,39 +40,23 @@ data(.gapminder, package="gapminder")
     .problem_number <<- 1
     
     .q1_correct <- 
-      ggplot(.gap_US, 
+      ggplot(gap_US, 
              mapping = aes(x = year, 
                            y = gdpPercap)) +
       geom_line() 
     
+    gg_req <- .q1_correct
+    gg_ans <- q1
+    
     .autograder <<-
       function(){
-        if (!is.ggplot(q1)) return(c(value = -1, message = "Your result should be a ggplot2 object."))
+        if (!is.ggplot(gg_ans)) return(c(value = -1, message = "Wrong. Your result should be a ggplot2 object. Please try again."))
         
-        # test 1
-        # that data used is correct
-        .q1_test1 <- all_equal(
-          target = as_tibble(q1$data), 
-          current = as_tibble(.q1_correct$data))
+        # Use compare_ggplots to check if answer is perfectly correct
+        perfect_match <- suppressWarnings(compare_ggplots(gg_req, gg_ans))
         
-        # test 2
-        # that learner used geom_line()
-        .q1_test2 <- any(stringr::str_detect(capture.output(q1$layers), 
-                                             "geom_line"))
-        # test 3
-        # check the x mapping
-        .q1_test3 <- "* `x` -> `year`" %in% capture.output(q1$mapping)
-        
-        # test 4
-        # check the y mapping
-        .q1_test4 <- "* `y` -> `gdpPercap`" %in% capture.output(q1$mapping)
-        
-        if (isTRUE(!(.q1_test1))) return(c(value = 0, message = "Wrong! Check which dataset you are plotting."))
-        if (isTRUE(!(.q1_test2))) return(c(value = 0, message = "Wrong! Do not forget to use ggplot2 geometry function geom_line."))
-        if (isTRUE(!(.q1_test3 && .q1_test4))) return(c(value = 0, message = "Wrong! Check your mapping arguments for x and y: are you putting the right variables?"))
-        if (isTRUE(.q1_test1 && .q1_test2 && .q1_test3 && .q1_test4)) return(c(value = 1, message = paste("Correct!", praise::praise()) ))
-        # wrong
-        return(c(value = 0, message = "Wrong. Please try again."))
+        if(isTRUE(perfect_match)) return(c(value = 1, message = paste("Correct!", praise::praise()) ))
+        if(!isTRUE(perfect_match)) return(c(value = 0, message = "Wrong. Please check the hint and try again."))
       }
     .apply_autograder()
   }
@@ -108,60 +91,26 @@ data(.gapminder, package="gapminder")
     .problem_number <<- 2
     
     .q2_correct <- 
-      ggplot(.gap_US, 
+      ggplot(gap_US, 
              mapping = aes(x = year, 
                            y = gdpPercap)) +
       geom_line(size = 1.5, 
                 color = "grey") +
       geom_point(size = 3, 
-                 color = "forestgreen")
+                 color = "forestgreen") 
     
-    # to access color, size of SOLUTION
-    .q2_correct_build <- ggplot_build(.q2_correct)
+    gg_req <- .q2_correct
+    gg_ans <- q2
     
     .autograder <<-
       function(){
-        if (!is.ggplot(q2)) return(c(value = -1, message = "Your result should be a ggplot2 object."))
+        if (!is.ggplot(gg_ans)) return(c(value = -1, message = "Wrong. Your result should be a ggplot2 object. Please try again."))
         
-        # to access color, size of ANSWER
-        .q2_build <- ggplot_build(q2)
+        # Use compare_ggplots to check if answer is perfectly correct
+        perfect_match <- suppressWarnings(compare_ggplots(gg_req, gg_ans))
         
-        # test 1
-        # that learner used geom_line()
-        .q2_test1 <- any(stringr::str_detect(capture.output(q2$layers), 
-                                             "geom_line"))
-        
-        # test 2
-        # that learner used geom_point()
-        .q2_test2 <- any(stringr::str_detect(capture.output(q2$layers), 
-                                             "geom_point"))
-        
-        # test 3
-        # check the color argument of geom_line
-        .q2_test3 <- .q2_build$data[[1]]["colour"][1,1] == .q2_correct_build$data[[1]]["colour"][1,1]
-        
-        # test 4
-        # check the size argument of geom_line
-        .q2_test4 <- .q2_build$data[[1]]["size"][1,1] == .q2_correct_build$data[[1]]["size"][1,1]
-        
-        # test 5
-        # check the color argument of geom_point
-        .q2_test5 <- .q2_build$data[[2]]["colour"][1,1] == .q2_correct_build$data[[2]]["colour"][1,1]
-        
-        # test 6
-        # check the size argument of geom_point
-        .q2_test6 <- .q2_build$data[[2]]["size"][1,1] == .q2_correct_build$data[[2]]["size"][1,1]
-        
-        if (isTRUE(!(.q2_test1))) return(c(value = 0, message = "! Do not forget to use ggplot2 geometry function geom_line"))
-        if (isTRUE(!(.q2_test2))) return(c(value = 0, message = "! Do not forget to use ggplot2 geometry function geom_point"))
-        if (isTRUE(!(.q2_test3))) return(c(value = 0, message = "! Check your color argument for geom_line"))
-        if (isTRUE(!(.q2_test4))) return(c(value = 0, message = "! Check your size argument for geom_line"))
-        if (isTRUE(!(.q2_test5))) return(c(value = 0, message = "! Check your color argument for geom_point"))
-        if (isTRUE(!(.q2_test6))) return(c(value = 0, message = "! Check your size argument for geom_point"))
-        
-        if (isTRUE(.q2_test1 && .q2_test2 && .q2_test3 && .q2_test4 && .q2_test5 && .q2_test6)) return(c(value = 1, message = paste("Correct!", praise::praise()) ))
-        # wrong
-        return(c(value = 0, message = "Wrong. Please try again."))
+        if(isTRUE(perfect_match)) return(c(value = 1, message = paste("Correct!", praise::praise()) ))
+        if(!isTRUE(perfect_match)) return(c(value = 0, message = "Wrong. Please check the hint and try again."))
       }
     .apply_autograder()
   }
@@ -170,7 +119,7 @@ data(.gapminder, package="gapminder")
 # create one hint per question
 .HINT_q2 <- function(){
   'We want to plot a line and points: you need to use two different geometry functions.
-  Then we want to customize both geometries with their own color and size.' -> out
+  Then we want to customize the color and size as fixed aesthetics inside each geom layer.' -> out
   cat(out)
 }
 # solution of question
@@ -182,6 +131,350 @@ data(.gapminder, package="gapminder")
                 color = "grey") +
       geom_point(size = 3, 
                  color = "forestgreen")' -> out
+  cat(out)
+}
+
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## q3 ----
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#Using the `gap_mini` data frame, create a **population** growth chart with these aesthetic mappings:
+
+# [backend]
+.CHECK_q3 <-
+  function() {
+    .problem_number <<- 3
+    
+    .q3_correct <- 
+      ggplot(gap_mini,
+             aes(x = year,
+                 y = pop,
+                 color = country,
+                 linetype = country)) +
+      geom_line()
+    
+    gg_req <- .q3_correct
+    gg_ans <- q3
+    
+    .autograder <<-
+      function(){
+        if (!is.ggplot(gg_ans)) return(c(value = -1, message = "Wrong. Your result should be a ggplot2 object. Please try again."))
+        
+        # Use compare_ggplots to check if answer is perfectly correct
+        perfect_match <- suppressWarnings(compare_ggplots(gg_req, gg_ans))
+        
+        if(isTRUE(perfect_match)) return(c(value = 1, message = paste("Correct!", praise::praise()) ))
+        if(!isTRUE(perfect_match)) return(c(value = 0, 
+                                            message = "Wrong. Please check the hint and try again."))
+      }
+    .apply_autograder()
+  }
+
+# [backend]
+# create one hint per question
+.HINT_q3 <- function(){
+  "See axis labels to check which variables are mapped to x and y.
+  See the guide on the right side of the plot to check which additional 
+  aesthetics were mapped to `country`. There are two shown in the plot.
+  Make sure you don't add additional modifications that the question did not ask for, or the CHECK function will deem it incorrect." -> out
+  cat(out)
+}
+# solution of question
+.SOLUTION_q3 <- function(){
+  'ggplot(gap_mini,
+             aes(x = year,
+                 y = pop,
+                 color = country,
+                 linetype = country)) +
+      geom_line()' -> out
+  cat(out)
+}
+
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## q4 ----
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#Next, add a layer of points to the previous plot, and add the required aesthetic mappings to produce a plot that looks like this:
+
+# [backend]
+.CHECK_q4 <-
+  function() {
+    .problem_number <<- 4
+    
+    .q4_correct <- 
+      ggplot(gap_mini,
+             aes(x = year,
+                 y = pop,
+                 color = country,
+                 shape = continent,
+                 lty = country)) +
+      geom_line() +
+      geom_point()
+    
+    gg_req <- .q4_correct
+    gg_ans <- q4
+    
+    .autograder <<-
+      function(){
+        if (!is.ggplot(gg_ans)) return(c(value = -1, message = "Wrong. Your result should be a ggplot2 object. Please try again."))
+        
+        # Use compare_ggplots to check if answer is perfectly correct
+        perfect_match <- suppressWarnings(compare_ggplots(gg_req, gg_ans))
+        
+        if(isTRUE(perfect_match)) return(c(value = 1, message = paste("Correct!", praise::praise()) ))
+        if(!isTRUE(perfect_match)) return(c(value = 0, 
+                                            message = "Wrong. Please check the hint and try again."))
+      }
+    .apply_autograder()
+  }
+
+# [backend]
+# create one hint per question
+.HINT_q4 <- function(){
+  "Take the correct code from q3 and add one additional mapping to make the plot for q4.
+  Make sure you don't add additional modifications that the question did not ask for, or the CHECK function will deem it incorrect." -> out
+  cat(out)
+}
+# solution of question
+.SOLUTION_q4 <- function(){
+  'ggplot(gap_mini,
+             aes(x = year,
+                 y = pop,
+                 color = country,
+                 shape = continent,
+                 lty = country)) +
+      geom_line() +
+      geom_point()' -> out
+  cat(out)
+}
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## q5 ----
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Next, add a layer of points to the previous plot, and add the required aesthetic mappings to produce a plot that looks like this:
+
+# [backend]
+.CHECK_q5 <-
+  function() {
+    .problem_number <<- 5
+    
+    .q5_correct <- 
+      ggplot(data = gap_mini2, 
+             mapping = aes(x = year, 
+                           y = gdpPercap, 
+                           color = country)) +
+      geom_line(size = 1) +
+      scale_x_continuous(breaks = gap_years) +
+      scale_y_continuous(breaks = seq(from = 1000, to = 7000, by = 1000))
+    
+    gg_req <- .q5_correct
+    gg_ans <- q5
+    
+    .autograder <<-
+      function(){
+        if (!is.ggplot(gg_ans)) return(c(value = -1, message = "Wrong. Your result should be a ggplot2 object. Please try again."))
+        
+        # Use compare_ggplots to check if answer is perfectly correct
+        perfect_match <- suppressWarnings(compare_ggplots(gg_req, gg_ans))
+        
+        if(isTRUE(perfect_match)) return(c(value = 1, message = paste("Correct!", praise::praise()) ))
+        if(!isTRUE(perfect_match)) return(c(value = 0, 
+                                            message = "Wrong. Please check the hint and try again."))
+      }
+    .apply_autograder()
+  }
+
+# [backend]
+# create one hint per question
+.HINT_q5 <- function(){
+  "
+  Make sure you don't add additional modifications that the question did not ask for, or the CHECK function will deem it incorrect." -> out
+  cat(out)
+}
+# solution of question
+.SOLUTION_q5 <- function(){
+  'ggplot(data = gap_mini2, 
+       mapping = aes(x = year, 
+                     y = gdpPercap, 
+                     color = country)) +
+  geom_line(size = 1) +
+  scale_x_continuous(breaks = gap_years) +
+  scale_y_continuous(breaks = seq(from = 1000, to = 7000, by = 1000))' -> out
+  cat(out)
+}
+
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## q6 ----
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# First subset `gapminder` to only the rows containing data for **Uganda:**
+# use **`gap_Uganda`** to create a time series plot of population (**`pop`**) over time (**`year`**). Transform the y axis to a log scale, edit the scale breaks to **`gap_years`**, change the line color to `forestgreen` and the size to 1mm.
+
+# [backend]
+.CHECK_q6 <-
+  function() {
+    .problem_number <<- 6
+    
+    .q6_correct <- 
+      ggplot(data = gap_Uganda, aes(x = year, y = pop)) + 
+      geom_line(size = 1, color = "forestgreen")+
+      scale_x_continuous(breaks = gap_years) +
+      scale_y_log10()
+    
+    gg_req <- .q6_correct
+    gg_ans <- q6
+    
+    .autograder <<-
+      function(){
+        if (!is.ggplot(gg_ans)) return(c(value = -1, message = "Wrong. Your result should be a ggplot2 object. Please try again."))
+        
+        # Use compare_ggplots to check if answer is perfectly correct
+        perfect_match <- suppressWarnings(compare_ggplots(gg_req, gg_ans))
+        
+        if(isTRUE(perfect_match)) return(c(value = 1, message = paste("Correct!", praise::praise()) ))
+        if(!isTRUE(perfect_match)) return(c(value = 0, 
+                                            message = "Wrong. Please check the hint and try again."))
+      }
+    .apply_autograder()
+  }
+
+# [backend]
+# create one hint per question
+.HINT_q6 <- function(){
+  'First, check that your essential layers are correct dataframe (gap_Uganda), aesthetic mappings, and geom function.
+   You need to make two scale modifications: x-axis scale breaks set to match the years in the dataset, 
+  and transform the y-axis scale to logarithmic (use the scale function we just learned).
+   Add fixed aesthetics color = "foestgreen", and  "size = 1" in geom_line(), but make sure not to add additional modifications that the question did not ask for, or the CHECK function will deem it incorrect.' -> out
+  cat(out)
+}
+# solution of question
+.SOLUTION_q6 <- function(){
+  'ggplot(data = gap_Uganda, mapping = aes(x = year, y = pop)) + 
+      geom_line(size = 1, color = "forestgreen")+
+      scale_x_continuous(breaks = gap_years) +
+      scale_y_log10()' -> out
+  cat(out)
+}
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## q7 ----
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# [backend]
+.CHECK_q7 <-
+  function() {
+    .problem_number <<- 7
+    
+    .q7_correct <- 
+      ggplot(data = my_gap_mini, 
+             mapping = aes(y = lifeExp, 
+                           x = year, 
+                           color = country)) +
+      geom_line(size = 1, alpha = 0.5) +
+      geom_point(size = 2) +
+      scale_x_continuous(breaks = gap_years)
+    
+    gg_req <- .q7_correct
+    gg_ans <- q7
+    
+    .autograder <<-
+      function(){
+        if (!is.ggplot(gg_ans)) return(c(value = -1, message = "Wrong. Your result should be a ggplot2 object. Please try again."))
+        
+        # Use compare_ggplots to check if answer is perfectly correct
+        perfect_match <- suppressWarnings(compare_ggplots(gg_req, gg_ans))
+        
+        if(isTRUE(perfect_match)) return(c(value = 1, message = paste("Correct!", praise::praise()) ))
+        if(!isTRUE(perfect_match)) return(c(value = 0, 
+                                            message = "Wrong. Please check the hint and try again."))
+      }
+    .apply_autograder()
+  }
+
+# [backend]
+# create one hint per question
+.HINT_q7 <- function(){
+  "
+  Make sure you don't add additional modifications that the question did not ask for, or the CHECK function will deem it incorrect." -> out
+  cat(out)
+}
+# solution of question
+.SOLUTION_q7 <- function(){
+  'ggplot(data = my_gap_mini, 
+       mapping = aes(y = lifeExp, 
+                     x = year, 
+                     color = country)) +
+  geom_line(size = 1, alpha = 0.5) +
+  geom_point(size = 2) +
+  scale_x_continuous(breaks = gap_years)' -> out
+  cat(out)
+}
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## q8 ----
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# [backend]
+.CHECK_q8 <-
+  function() {
+    .problem_number <<- 8
+    
+    .q8_correct <- 
+      ggplot(data = my_gap_mini, 
+             mapping = aes(y = lifeExp, 
+                           x = year, 
+                           color = country)) +
+      geom_line(size = 1, alpha = 0.5) +
+      geom_point(size = 2) +
+      scale_x_continuous(breaks = gap_years) +
+      labs(x = "Year", 
+           y = "Longevity",
+           title = "Health & wealth of nations",
+           subtitle = "China, India, and Thailand (1952-2007)",
+           color = "Color")
+    
+    gg_req <- .q8_correct
+    gg_ans <- q8
+    
+    .autograder <<-
+      function(){
+        if (!is.ggplot(gg_ans)) return(c(value = -1, message = "Wrong. Your result should be a ggplot2 object. Please try again."))
+        
+        # Use compare_ggplots to check if answer is perfectly correct
+        perfect_match <- suppressWarnings(compare_ggplots(gg_req, gg_ans))
+        
+        if(isTRUE(perfect_match)) return(c(value = 1, message = paste("Correct!", praise::praise()) ))
+        if(!isTRUE(perfect_match)) return(c(value = 0, 
+                                            message = "Wrong. Please check the hint and try again."))
+      }
+    .apply_autograder()
+  }
+
+# [backend]
+# create one hint per question
+.HINT_q8 <- function(){
+  "
+  Make sure you don't add additional modifications that the question did not ask for, or the CHECK function will deem it incorrect." -> out
+  cat(out)
+}
+# solution of question
+.SOLUTION_q8 <- function(){
+  'ggplot(data = my_gap_mini, 
+       mapping = aes(y = lifeExp, 
+                     x = year, 
+                     color = country)) +
+  geom_line(size = 1, alpha = 0.5) +
+  geom_point(size = 2) +
+  scale_x_continuous(breaks = gap_years) +
+  labs(x = "Year", 
+       y = "Longevity",
+       title = "Health & wealth of nations",
+       subtitle = "China, India, and Thailand (1952-2007)",
+       color = "Color")' -> out
   cat(out)
 }
 
